1f82a4c86a6d87db104dde3c34014152db7f074f
teybhacc <dylantey97@gmail.com>
2023-03-28 15:04:18 +0800

    uat autodeploy test

---
+++ autodeploy-prod-uat.sh
@@ -0,0 +1,22 @@
+ACCOUNT=395321600628
+REGION=ap-southeast-1
+SECRET_NAME=smarttradzt-ecr
+EMAIL=raja.r@smarttradzt.com
+NAMESPACE=pricing-tool-int
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+echo $TOKEN
+
+kubectl config set-context kubernetes-admin@kubernetes --namespace=$NAMESPACE
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
+
+rm -rf iocl.zip
+rm -rf iocl
+wget https://github.com/acceval/temp-iocl/raw/main/iocl.zip
+unzip iocl.zip
+kubectl apply -k iocl/pricing-tool-uat/overlays/production
